5c08247ec55d1ad949ebb664dedfab09400b2a63
billtangts@gmail.com <billtangts@gmail.com>
2020-09-02 09:26:26 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -54,5 +54,3 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
-latex_elements = {     'classoptions': ',openany,oneside',     'babel' : '\\usepackage[polish]{babel}' }
